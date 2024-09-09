163b7671a8236181de7011769e5b2d69821fbd4e
jrr603 <jrr603@nyu.edu>
2024-09-09 12:35:31 -0400

    Create plot.gaussian.py

---
+++ ps_1/plot.gaussian.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu  = 0
+std = 3
+
+variance = np.square(std)
+
+x = np.linspace(-10,10,1000)
+f = np.exp(-np.square(x-mu)/(2*variance))/(np.sqrt(2*np.pi*variance))
+
+plt.plot(x,f)
+plt.xlabel('x')
+plt.xlabel('y')
+plt.title('gaussian')
+plt.show()
